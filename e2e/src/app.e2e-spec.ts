c2b48ae655130968bd7f6313785da54b06bd054b
Boris <boris.grevtsov@ihsmarkit.com>
2020-10-16 10:00:06 +0300

    App Init

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('ngrx-github app is running!');
+    expect(page.getTitleText()).toEqual('ngrx-wallpapers app is running!');
   });
 
   afterEach(async () => {
