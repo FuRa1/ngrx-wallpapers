cb3096cc1bdad557658a8c6679bd2f101958803e
Boris <boris.grevtsov@ihsmarkit.com>
2020-11-13 00:35:09 +0200

    Auto-generated commit

--- src/app/app.state.ts
+++
@@ -1,8 +0,0 @@
-import { IWallpaper } from './models/wallpaper.model';
-
-export interface IAppState {
-  readonly recent: IWallpaper[]; // TODO refactor to { [id: number]: IWallpaper }
-  readonly favorites: IWallpaper[];
-  readonly recentLoading: boolean;
-  readonly recentLoaded: boolean;
-}
